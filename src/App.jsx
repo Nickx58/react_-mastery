074f505453b7b74ed62ec610e240654884dee55f
Nickx58 <nikhilx58@gmail.com>
2025-06-15 23:16:49 +0530

    Add styles from server

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,24 @@ import Pizza from "./Pizza";
 const App = () => {
   return (
     <div>
-      <h1>Padre Gino's - Order now</h1>
-      <Pizza name="four cheese" description="Best cheesse pizza" />
+      <h1 className="logo">Padre Gino's Pizza</h1>
+      <Pizza
+        name="Pepperoni"
+        description="Mozzarella Cheese, Pepperoni"
+        image={"/public/pizzas/pepperoni.webp"}
+      />
+      <Pizza
+        name="The Hawaiian Pizza"
+        description="Sliced Ham, Pineapple, Mozzarella Cheese"
+        image={"/public/pizzas/hawaiian.webp"}
+      />
+      <Pizza
+        name="The Big Meat Pizza"
+        description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage"
+        image={"/public/pizzas/big_meat.webp"}
+      />
     </div>
-  )
+  );
 };
 
 const container = document.getElementById("root");

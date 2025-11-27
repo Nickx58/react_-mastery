38944d1dcfc09f49149c597b4d774bf5a79c95f8
Nickx58 <nikhilx58@gmail.com>
2025-11-27 23:13:26 +0530

    React: Add static files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
 import Pizza from "./Pizza";
-import Order from "./Order";
+// import Order from "./Order";
 
 const App = () => {
   return (
     <div>
       <h1 className="logo">Padre Gino's Pizza</h1>
-      {/* <Pizza
+      <Pizza
         name="Pepperoni"
         description="Mozzarella Cheese, Pepperoni"
         image={"/public/pizzas/pepperoni.webp"}
@@ -20,8 +20,8 @@ const App = () => {
         name="The Big Meat Pizza"
         description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage"
         image={"/public/pizzas/big_meat.webp"}
-      /> */}
-      <Order />
+      />
+      {/* <Order /> */}
     </div>
   );
 };
